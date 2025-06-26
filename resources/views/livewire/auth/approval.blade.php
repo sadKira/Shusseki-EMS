7c47b34c0c7e302393aabc8f96179ec8f9dee0d7
sadKira <ibizapooled@gmail.com>
2025-06-26 19:47:56 +0800

    checkpoint

--- resources/views/livewire/auth/approval.blade.php
+++ resources/views/livewire/auth/approval.blade.php
@@ -1,10 +1,10 @@
 <div class="flex flex-col gap-6">
     <div class="flex w-full flex-col text-center">
-        <flux:heading size="xl">Your account is awaiting Approval</flux:heading>
-        <flux:subheading>LOOOOOL</flux:subheading>
+        <flux:heading size="xl" class="md:text-2xl mb-2">Hang Tight! We're Reviewing Your Account</flux:heading>
+        <flux:subheading class="md:text-base mb-6">Your account is under review. You wil receive an update as soon as it's approved</flux:subheading>
     </div>
 
-    <form method="POST" action="{{ route('logout') }}" class="flex items-center justify-end">
+    <form method="POST" action="{{ route('logout') }}" class="flex items-center justify-end md:w-auto">
       @csrf
       <flux:button variant="primary" type="submit" class="w-full">{{ __('Return') }}</flux:button>
     </form>
