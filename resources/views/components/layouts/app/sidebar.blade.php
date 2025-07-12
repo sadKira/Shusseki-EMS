58cfe722e5d76054eedf04c6ecf31d8d733c8774
sadKira <ibizapooled@gmail.com>
2025-07-12 17:22:20 +0800

    checkpoint

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -55,7 +55,7 @@
             <flux:navlist.group :heading="__('Events')" class="grid">
                 @can('manage')
                     <flux:navlist.item icon="calendar" :href="route('manage_events')"
-                        :current="request()->routeIs(['manage_events', 'create_event', 'all_events'])" wire:navigate>
+                        :current="request()->routeIs(['manage_events', 'create_event'])" wire:navigate>
                         {{ __('Manage Events') }}
                     </flux:navlist.item>
                     <flux:navlist.item icon="envelope" :href="route('coverage_events')"
