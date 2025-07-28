4cd0f78b80cfb7a49fd5902f22268ccd6916763b
sadKira <ibizapooled@gmail.com>
2025-07-29 01:55:23 +0800

    Update .gitignore to exclude Laravel framework storage files and remove cached view files from tracking

--- storage/framework/views/af6711e0c9b55ee0105325ea27231600.php
+++
@@ -1,6 +0,0 @@
-
-<div
-    class="z-10 fixed inset-0 bg-black/10 hidden in-data-show-stashed-sidebar:block lg:in-data-show-stashed-sidebar:hidden"
-    x-data x-on:click="document.body.removeAttribute('data-show-stashed-sidebar')"
-></div>
-<?php /**PATH C:\Users\monro\Herd\shusseki\vendor\livewire\flux\src/../stubs/resources/views/flux/sidebar/backdrop.blade.php ENDPATH**/ ?>
