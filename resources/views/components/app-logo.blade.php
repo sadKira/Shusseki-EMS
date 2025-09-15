85ac8a636ec708d92d28976620ede42883dfa303
sadKira <ibizapooled@gmail.com>
2025-09-15 23:40:09 +0800

    Production -fix Side 404

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,7 +1,7 @@
 <div class="flex items-center justify-center rounded-md">
     {{-- For Light Mode --}}
-    <img src="{{ asset('images/Side.svg') }}" alt="MKD Logo"
-        class="block dark:hidden h-13 w-auto sm:h-16 md:h-20">
+    {{-- <img src="{{ asset('images/Side.svg') }}" alt="MKD Logo"
+        class="block dark:hidden h-13 w-auto sm:h-16 md:h-20"> --}}
 
     {{-- For Dark Mode --}}
     <img src="{{ asset('images/Side_White.svg') }}" alt="MKD Logo"
