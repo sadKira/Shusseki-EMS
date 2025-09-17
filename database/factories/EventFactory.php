1f585ec598b9c67a816bd82ed37d223934ad1a9b
sadKira <ibizapooled@gmail.com>
2025-09-17 14:34:44 +0800

    Production -fixing images 3

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -68,12 +68,12 @@ class EventFactory extends Factory
                 ->format('H:i:s'),
             'school_year' => $schoolYear,
             'image' => fake()->randomElement([
-                'https://shusseki-ems.site/events/intrams.jfif',
-                'https://shusseki-ems.site/events/clubfair.jfif',
-                'https://shusseki-ems.site/events/galanight.jfif',
-                'https://shusseki-ems.site/events/nyuugakushiki.jfif',
-                'https://shusseki-ems.site/events/personnelsday.jfif',
-                'https://shusseki-ems.site/events/pjfest.jfif',
+                'https://shusseki-ems.site/app/public/events/intrams.jfif',
+                'https://shusseki-ems.site/app/public/events/clubfair.jfif',
+                'https://shusseki-ems.site/app/public/events/galanight.jfif',
+                'https://shusseki-ems.site/app/public/events/nyuugakushiki.jfif',
+                'https://shusseki-ems.site/app/public/events/personnelsday.jfif',
+                'https://shusseki-ems.site/app/public/events/pjfest.jfif',
             ]),
             'status' => fake()->randomElement([
                 'not_finished',
