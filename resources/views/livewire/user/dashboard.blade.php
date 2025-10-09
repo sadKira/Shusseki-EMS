0a0af8ad7b7190073d1d6403de6e55b004d7c89d
agneralain-dev <alainglennagner17@gmail.com>
2025-10-09 11:32:15 +0800

    Production -stable 1.25.10.27

--- resources/views/livewire/user/dashboard.blade.php
+++ resources/views/livewire/user/dashboard.blade.php
@@ -496,7 +496,7 @@
                                                                 </flux:badge>
                                                             @elseif($event->status == \App\Enums\EventStatus::NotFinished)
                                                                 @if ($now->between($start, $end))
-                                                                    <flux:badge color="amber" class="mr-10" variant="solid">
+                                                                    <flux:badge color="amber" class="" variant="solid">
                                                                         <span class="text-black">In Progress</span>
                                                                     </flux:badge>
                                                                 @else
@@ -513,7 +513,7 @@
                                                         @else
                                                             @if($event->status == \App\Enums\EventStatus::NotFinished)
                                                                 @if ($now->between($start, $end))
-                                                                    <flux:badge color="amber" class="mr-10" variant="solid">
+                                                                    <flux:badge color="amber" class="" variant="solid">
                                                                         <span class="text-black">In Progress</span>
                                                                     </flux:badge>
                                                                 @else
